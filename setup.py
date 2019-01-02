c91d06e7db11cd038e61ded7eee35675b5f26d25
Revanth Yesireddy <revanthyesireddy@Revanths-MacBook-Pro.local>
2019-01-02 15:39:30 -0500

    random test

--- setup.py
+++ setup.py
@@ -215,7 +215,7 @@ setuptools.setup(
     license='BSD',
     platforms=['any'],
     install_requires=install_requires(),
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.4.*",
     tests_require=reqs('test.txt'),
     extras_require=extras_require(),
     classifiers=[s.strip() for s in classes.split('\n') if s],
